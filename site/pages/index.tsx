be2c3d53294d781e70a61c1d6495410214776d62
Prashi Kapoor <prakapoo0@WKMZTA450450.global.publicisgroupe.net>
2023-03-25 20:22:32 +0000

    added another hero

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -42,6 +42,10 @@ export default function Home({
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
+      <Hero
+        headline=" Dessert dragée halvah croissant."
+        description="Cupcake ipsum dolor sit amet lemon drops pastry cotton candy. Sweet carrot cake macaroon bonbon croissant fruitcake jujubes macaroon oat cake. Soufflé bonbon caramels jelly beans. Tiramisu sweet roll cheesecake pie carrot cake. "
+      />
       <Grid variant="filled">
         {products.slice(0, 3).map((product: any, i: number) => (
           <ProductCard
